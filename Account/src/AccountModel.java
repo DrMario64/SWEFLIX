aac523d11f665ee2a21904c30c4dd36867805215
DrMario64 <lucasalm05@hotmail.com>
2021-11-04 11:14:26 -0400

    added getAccounts(username)

--- Account/src/AccountModel.java
+++ Account/src/AccountModel.java
@@ -2,16 +2,25 @@ public class AccountModel {
     private String account;
 
     /*
-     * gets an account from the database given the account ID
+     * gets an account from the database given the account ID.
+     * still need to make account object
      */
-    /*public String getAccount(String accountID) {
+    /*public Account getAccount(String accountID) {
         account = searchDatabase(accountID);
     }*/
 
     /*
      * retrieves all accounts from the database and returns an array of them
+     * still need to make account object
      */
-    /*public String[] getAccountList() {
+    /*public Account[] getAccountList() {
         return getAllAccounts();
+    }*/
+
+    /*
+     * retrieves all accounts that share the same username
+     */
+    /*public Account[] getAccounts(String username) {
+        return getAccountsByUsername(username);
     }*/
 }

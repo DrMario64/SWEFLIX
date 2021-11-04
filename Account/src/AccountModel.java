62786cfb81e912bf741410a41d2a090e33a48f86
DrMario64 <lucasalm05@hotmail.com>
2021-11-04 00:26:41 -0400

    first commit
    Account Model-View

---
+++ Account/src/AccountModel.java
@@ -0,0 +1,17 @@
+public class AccountModel {
+    private String account;
+
+    /*
+     * gets an account from the database given the account ID
+     */
+    /*public String getAccount(String accountID) {
+        account = searchDatabase(accountID);
+    }*/
+
+    /*
+     * retrieves all accounts from the database and returns an array of them
+     */
+    /*public String[] getAccountList() {
+        return getAllAccounts();
+    }*/
+}

262daf98fb5254963925a733d9ee9fc594ecd235
acewatz <56747137+acewatz@users.noreply.github.com>
2021-12-02 10:50:45 -0500

    update movies

--- Movies/src/main/java/com/springboot/movies/MovieController.java
+++ Movies/src/main/java/com/springboot/movies/MovieController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/")
+@RequestMapping("/movie")
 @RestController
 public class MovieController {
 	private final MovieService movieService;
--- Movies/src/main/java/com/springboot/movies/MoviesApplication.java
+++ Movies/src/main/java/com/springboot/movies/MoviesApplication.java
@@ -1,13 +1,43 @@
 package com.springboot.movies;
 
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+
 
 @SpringBootApplication
 public class MoviesApplication {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(MoviesApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
-
+	
+	@Autowired
+	MovieService movieService;
+	
+	@Bean
+	public CommandLineRunner demo(MovieDao movieDao) {
+		return (args) -> {
+			
+			//creates and initializes a new movie then adds to repository
+			Movie movie1 = new Movie(UUID.randomUUID(), "Titanic");
+			movieDao.addMovie(movie1);
+			
+			//display movie info
+			log.info("\nID:" +  movie1.getId() + "\nMovie: " + movie1.getName()); 
+			
+		};
+	}
 }
